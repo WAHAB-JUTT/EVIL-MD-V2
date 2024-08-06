e07626f024dbb619778d985e096b6b68bbb19d46
Dark Evil <120171162+Itxxevil@users.noreply.github.com>
2024-08-07 00:11:21 +0500

    𝐄𝐕𝐈𝐋-𝐌𝐃-𝐕2

---
+++ src/plugin/apkdown.js
@@ -0,0 +1,159 @@
+import { search, download } from 'aptoide-scraper';
+import pkg, { prepareWAMessageMedia } from '@whiskeysockets/baileys';
+const { generateWAMessageFromContent, proto } = pkg;
+
+const apkMap = new Map();
+let apkIndex = 1; // Global index for APKs
+
+const searchAPK = async (m, Matrix) => {
+  let selectedListId;
+  const selectedButtonId = m?.message?.templateButtonReplyMessage?.selectedId;
+  const interactiveResponseMessage = m?.message?.interactiveResponseMessage;
+
+  if (interactiveResponseMessage) {
+    const paramsJson = interactiveResponseMessage.nativeFlowResponseMessage?.paramsJson;
+    if (paramsJson) {
+      const params = JSON.parse(paramsJson);
+      selectedListId = params.id;
+    }
+  }
+
+  const selectedId = selectedListId || selectedButtonId;
+
+  const prefixMatch = m.body.match(/^[\\/!#.]/);
+  const prefix = prefixMatch ? prefixMatch[0] : '/';
+  const cmd = m.body.startsWith(prefix) ? m.body.slice(prefix.length).split(' ')[0].toLowerCase() : '';
+  const text = m.body.slice(prefix.length + cmd.length).trim();
+
+  const validCommands = ['apk', 'searchapk', 'apkdl', 'app'];
+
+  if (validCommands.includes(cmd)) {
+    if (!text) return m.reply('Please provide a search query for APKs');
+
+    try {
+      await m.React("🕘");
+
+
+      let searchResult = await search(text);
+      const topAPKs = searchResult.slice(0, 10);
+
+      if (topAPKs.length === 0) {
+        m.reply('No APKs found.');
+        await m.React("❌");
+        return;
+      }
+
+      const apkButtons = await Promise.all(topAPKs.map(async (apk, index) => {
+        const uniqueId = `apk_${apkIndex + index}`;
+        const apkDetails = await download(apk.id); 
+        apkMap.set(uniqueId, {
+          ...apk,
+          size: apkDetails.size 
+        });
+        return {
+          "header": "",
+          "title": `📥 ${apk.name}`, 
+          "description": `Size: ${apkDetails.size}`,
+          "id": uniqueId 
+        };
+      }));
+
+      const msg = generateWAMessageFromContent(m.from, {
+        viewOnceMessage: {
+          message: {
+            messageContextInfo: {
+              deviceListMetadata: {},
+              deviceListMetadataVersion: 2
+            },
+            interactiveMessage: proto.Message.InteractiveMessage.create({
+              body: proto.Message.InteractiveMessage.Body.create({
+                text: `*_ᴇᴠɪʟ-ᴍᴅ ᴀᴘᴋ ᴅᴏᴡɴʟᴏᴀᴅᴇʀ_*
+╭────────────────────━┈⊷
+│▸ 🔍 Download your favorite APKs.
+│
+│▸ 📌 Select APK from the below list.
+╰────────────────────━┈⊷ `
+              }),
+              footer: proto.Message.InteractiveMessage.Footer.create({
+                text: "*_───•◈ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴇᴠɪʟ ᴍᴅ²⁰²⁴◈•───_*"
+              }),
+              header: proto.Message.InteractiveMessage.Header.create({
+                ...(await prepareWAMessageMedia({ image: { url: `https://telegra.ph/file/1271d54767e6d0c21855a.jpg` } }, { upload: Matrix.waUploadToServer })),
+                title: ``,
+                gifPlayback: true,
+                subtitle: "",
+                hasMediaAttachment: false 
+              }),
+              nativeFlowMessage: proto.Message.InteractiveMessage.NativeFlowMessage.create({
+                buttons: [
+                  {
+                    name: "single_select",
+                    buttonParamsJson: JSON.stringify({
+                      title: "🔖 Select an APK",
+                      sections: [
+                        {
+                          title: "Top 10 APK Results",
+                          highlight_label: "Top 10",
+                          rows: apkButtons
+                        },
+                      ]
+                    })
+                  }
+                ],
+              }),
+              contextInfo: {
+                mentionedJid: [m.sender],
+                forwardingScore: 9999,
+                isForwarded: true,
+              }
+            }),
+          },
+        },
+      }, {});
+
+      await Matrix.relayMessage(msg.key.remoteJid, msg.message, {
+        messageId: msg.key.id
+      });
+      await m.React("✅");
+
+
+      apkIndex += topAPKs.length;
+    } catch (error) {
+      console.error("Error processing your request:", error);
+      m.reply('Error processing your request.');
+      await m.React("❌");
+    }
+  } else if (selectedId) { 
+    const selectedAPK = apkMap.get(selectedId);
+
+    if (selectedAPK) {
+      try {
+        const apkDetails = await download(selectedAPK.id); 
+        const url = apkDetails.dllink;
+        const iconUrl = apkDetails.icon;
+        const size = apkDetails.size;
+
+        await Matrix.sendMessage(m.from, { image: { url: iconUrl }, caption: `╭────────────────────━┈⊷
+│▸ You selected this APK:
+│▸ Name: ${selectedAPK.name}
+│▸ size: ${size}
+╰────────────────────━┈⊷> *_© ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴇᴠɪʟ ᴍᴅ²⁰²⁴_*` }, { quoted: m });
+
+  
+        const apkMessage = {
+          document: { url },
+          mimetype: 'application/vnd.android.package-archive',
+          fileName: `${selectedAPK.name}.apk`
+        };
+
+        await Matrix.sendMessage(m.from, apkMessage, { quoted: m });
+      } catch (error) {
+        console.error("Error sending APK:", error);
+        m.reply('Error sending APK.');
+      }
+    } else {
+    }
+  }
+};
+
+export default searchAPK;
---
+++ src/plugin/mediafiredl.js
@@ -0,0 +1,48 @@
+import axios from 'axios';
+import { mediafireDl } from 'mfiredlcore-vihangayt';
+
+const mediafireDownload = async (m, Matrix) => {
+  const prefixMatch = m.body.match(/^[\\/!#.]/);
+  const prefix = prefixMatch ? prefixMatch[0] : '/';
+  const cmd = m.body.startsWith(prefix) ? m.body.slice(prefix.length).split(' ')[0].toLowerCase() : '';
+  const text = m.body.slice(prefix.length + cmd.length).trim();
+
+  const validCommands = ['mediafire', 'mf', 'mfdownload'];
+
+  if (validCommands.includes(cmd)) {
+    if (!text) return m.reply('Please provide a MediaFire URL.');
+
+    try {
+      await m.React('⬆️');
+
+      const mediafireUrl = text;
+      const mediafireInfo = await mediafireDl(mediafireUrl);
+
+      if (mediafireInfo && mediafireInfo.link) {
+        const mediaUrl = mediafireInfo.link;
+        const caption = `*_ᴇᴠɪʟ-ᴍᴅ ᴍᴇᴅɪᴀғɪʀᴇ ᴅᴏᴡɴʟᴏᴀᴅᴇʀ_*
+╭────────────────────━┈⊷
+│▸📂 File: ${mediafireInfo.name}
+│▸🖲️ Size: ${mediafireInfo.size}
+│▸📅 Date: ${mediafireInfo.date}
+╰────────────────────━┈⊷`;
+
+
+        const extension = mediaUrl.split('.').pop().toLowerCase();
+
+
+        await Matrix.sendMedia(m.from, mediaUrl, extension, caption, m);
+
+        await m.React('✅');
+      } else {
+        throw new Error('Invalid response from MediaFire.');
+      }
+    } catch (error) {
+      console.error('Error downloading MediaFire file:', error.message);
+      m.reply('Error downloading MediaFire file.');
+      await m.React('❌');
+    }
+  }
+};
+
+export default mediafireDownload;
